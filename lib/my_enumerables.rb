82b873193e88ba124fd41b5f8cba9ddb331a2937
Shreyas <117995509+Philomath21@users.noreply.github.com>
2024-08-30 16:25:46 +0530

    completed

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,60 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index(&block)
+    i = 0
+    my_each do |element|
+      block.call(element, i)
+      i += 1
+    end
+  end
+
+  def my_select(&block)
+    selcted_a = []
+    my_each do |element|
+      selcted_a.push(element) if block.call(element)
+    end
+    selcted_a
+  end
+
+  def my_all?(&block)
+    my_each { |element| return false unless block.call(element) }
+    true
+  end
+
+  def my_any?(&block)
+    my_each {|element| return true if block.call(element)}
+    false
+  end
+
+  def my_none?(&block)
+    my_each {|element| return false if block.call(element)}
+    true
+  end
+
+  def my_count(&block)
+    return self.length unless block_given?
+
+    count = 0
+    my_each { |element| count += 1 if block.call(element) }
+    count
+  end
+
+  def my_map(&block)
+    map_a = []
+    my_each { |element| map_a.push(block.call(element)) }
+    map_a
+  end
+
+  def my_inject(memo = nil, &block)
+    my_each do |element|
+      if memo.nil?
+        memo = element
+      else
+        memo = block.call(memo, element)
+      end
+    end
+    memo
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +63,12 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each(&block)
+    i = 0
+    while i < length
+      block.call(self[i])
+      i += 1
+    end
+    self
+  end
 end
